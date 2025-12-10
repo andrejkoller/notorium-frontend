1599f8e15544e566014d9788f653fe0a52b957d4
Andrej Koller <andrejkoller@outlook.com>
2025-12-10 16:02:45 +0100

    refactor: rename sidebar classes for consistency and clarity

--- src/components/dashboard/dashboard-sidebar/dashboard-sidebar.tsx
+++ src/components/dashboard/dashboard-sidebar/dashboard-sidebar.tsx
@@ -4,28 +4,33 @@ import { FolderOpenIcon, HelpCircleIcon, SettingsIcon } from "lucide-react";
 
 function Sidebar() {
   return (
-    <div className="sidebar-container">
-      <div className="sidebar-header">
-        <h1 className="sidebar-title">Notorium</h1>
+    <div className="dashboard-sidebar-container">
+      <div className="dashboard-sidebar-header">
+        <Link to={"/"}>
+          <h1 className="dashboard-sidebar-title">Notorium</h1>
+        </Link>
       </div>
-      <div className="sidebar-body">
-        <ul className="sidebar-menu">
-          <li className="sidebar-menu-item">
-            <Link to={"/dashboard"} className="sidebar-menu-link">
-              <FolderOpenIcon className="sidebar-menu-icon" />
-              <span className="sidebar-menu-text">My Library</span>
+      <div className="dashboard-sidebar-body">
+        <ul className="dashboard-sidebar-menu">
+          <li className="dashboard-sidebar-menu-item">
+            <Link to={"/dashboard"} className="dashboard-sidebar-menu-link">
+              <FolderOpenIcon className="dashboard-sidebar-menu-icon" />
+              <span className="dashboard-sidebar-menu-text">My Library</span>
             </Link>
           </li>
-          <li className="sidebar-menu-item">
-            <Link to={"/dashboard/settings"} className="sidebar-menu-link">
-              <SettingsIcon className="sidebar-menu-icon" />
-              <span className="sidebar-menu-text">Settings</span>
+          <li className="dashboard-sidebar-menu-item">
+            <Link
+              to={"/dashboard/settings"}
+              className="dashboard-sidebar-menu-link"
+            >
+              <SettingsIcon className="dashboard-sidebar-menu-icon" />
+              <span className="dashboard-sidebar-menu-text">Settings</span>
             </Link>
           </li>
-          <li className="sidebar-menu-item">
-            <Link to={"/help"} className="sidebar-menu-link">
-              <HelpCircleIcon className="sidebar-menu-icon" />
-              <span className="sidebar-menu-text">Help</span>
+          <li className="dashboard-sidebar-menu-item">
+            <Link to={"/help"} className="dashboard-sidebar-menu-link">
+              <HelpCircleIcon className="dashboard-sidebar-menu-icon" />
+              <span className="dashboard-sidebar-menu-text">Help</span>
             </Link>
           </li>
         </ul>
