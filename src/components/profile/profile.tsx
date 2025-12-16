80a9bda5e9a37c61b3115935ced3590266544150
Andrej Koller <andrejkoller@outlook.com>
2025-12-16 10:40:03 +0100

    feat: add profile and banner image upload functionality in Profile component

--- src/components/profile/profile.tsx
+++ src/components/profile/profile.tsx
@@ -5,14 +5,17 @@ import {
   getSheetMusicFavorites,
 } from "../../services/sheet-music-service";
 import { Link } from "react-router-dom";
-import { Toaster } from "../ui/toaster";
 import SelectFilter from "../select-filter/select-filter";
 import { useSheetMusicContext } from "../../hooks/use-sheet-music";
 import type { SheetMusic } from "../../models/sheet-music";
 import "./profile.css";
+import {
+  uploadProfileImage,
+  uploadBannerImage,
+} from "../../services/user-service";
 
 function Profile() {
-  const { currentUser } = useCurrentUserContext();
+  const { currentUser, setCurrentUser } = useCurrentUserContext();
   const { sheetMusic, setSheetMusic } = useSheetMusicContext();
   const [loadingScores, setLoadingScores] = useState(true);
 
@@ -21,7 +24,7 @@ function Profile() {
   );
 
   const profileImageInputRef = useRef<HTMLInputElement | null>(null);
-  //const bannerImageInputRef = useRef<HTMLInputElement | null>(null);
+  const bannerImageInputRef = useRef<HTMLInputElement | null>(null);
 
   function formatGenre(genre: string) {
     return genre.replace(/([a-z])([A-Z])/g, "$1 $2");
@@ -31,7 +34,7 @@ function Profile() {
     profileImageInputRef.current?.click();
   };
 
-  /*const handleProfileImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleProfileImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (currentUser && e.target.files?.length) {
       const file = e.target.files[0];
       const formData = new FormData();
@@ -40,17 +43,9 @@ function Profile() {
       uploadProfileImage(currentUser.id, formData)
         .then((updatedUser) => {
           setCurrentUser({ ...currentUser, ...updatedUser });
-          toaster.success({
-            title: "Profile image updated",
-            description: "Your profile image has been successfully updated.",
-          });
         })
         .catch((error) => {
           console.error("Error uploading profile image:", error);
-          toaster.error({
-            title: "Error updating profile image",
-            description: "There was an error updating your profile image.",
-          });
         });
     }
   };
@@ -68,20 +63,12 @@ function Profile() {
       uploadBannerImage(currentUser.id, formData)
         .then((updatedUser) => {
           setCurrentUser({ ...currentUser, ...updatedUser });
-          toaster.success({
-            title: "Banner image updated",
-            description: "Your banner image has been successfully updated.",
-          });
         })
         .catch((error) => {
           console.error("Error uploading banner image:", error);
-          toaster.error({
-            title: "Error updating banner image",
-            description: "There was an error updating your banner image.",
-          });
         });
     }
-  };*/
+  };
 
   useEffect(() => {
     if (!currentUser?.id) return;
@@ -108,174 +95,179 @@ function Profile() {
   }, [currentUser?.id]);
 
   return (
-    <>
-      <div className="profile-container">
-        <div className="profile-content">
-          <div className="profile-header">
-            {currentUser ? (
-              <div className="profile-details">
-                <div
-                  className="profile-image"
-                  onClick={handleProfileImageClick}
-                >
-                  {currentUser.profileImage ? (
-                    <img
-                      src={`https://localhost:7189/${currentUser.profileImage}`}
-                      alt="Profile"
-                      className="profile-image"
-                    />
-                  ) : (
-                    <div className="profile-image-placeholder">
-                      <span>{currentUser.name?.charAt(0)}</span>
-                    </div>
-                  )}
-                </div>
-                <div className="profile-username">
-                  <p>Profile</p>
-                  <h2>{currentUser.username}</h2>
-                </div>
+    <div className="profile-container">
+      <input
+        type="file"
+        ref={profileImageInputRef}
+        onChange={handleProfileImageChange}
+        accept="image/*"
+        style={{ display: "none" }}
+      />
+      <input
+        type="file"
+        ref={bannerImageInputRef}
+        onChange={handleBannerImageChange}
+        accept="image/*"
+        style={{ display: "none" }}
+      />
+      <div className="profile-content">
+        <div className="profile-header">
+          {currentUser ? (
+            <div className="profile-details" onClick={handleBannerImageClick}>
+              <div className="profile-image" onClick={handleProfileImageClick}>
+                {currentUser.profileImage ? (
+                  <img
+                    src={`https://localhost:7189/${currentUser.profileImage}`}
+                    alt="Profile"
+                    className="profile-image"
+                  />
+                ) : (
+                  <div className="profile-image-placeholder">
+                    <span>{currentUser.name?.charAt(0)}</span>
+                  </div>
+                )}
               </div>
+              <div className="profile-username">
+                <p>Profile</p>
+                <h2>{currentUser.username}</h2>
+              </div>
+            </div>
+          ) : (
+            <p>Loading user information...</p>
+          )}
+        </div>
+        <div className="profile-body">
+          <div className="profile-scores">
+            <div className="profile-scores-wrapper">
+              <h3>Published Scores ({sheetMusic ? sheetMusic.length : 0})</h3>
+              <div className="profile-scores-filter">
+                <SelectFilter />
+              </div>
+            </div>
+            {loadingScores ? (
+              <p>Loading scores...</p>
+            ) : sheetMusic && sheetMusic.length > 0 ? (
+              <ul className="profile-scores-list">
+                {sheetMusic.map(
+                  (music) =>
+                    music.isPublic && (
+                      <li key={music.id} className="profile-scores-item">
+                        <Link
+                          to={`/dashboard/users/${music.user?.username}/scores/${music.id}`}
+                          className="profile-scores-link"
+                        >
+                          <img
+                            src={`https://localhost:7189/${music.previewImage}`}
+                            alt={music.title}
+                            className="profile-scores-image"
+                          />
+                        </Link>
+                        <Link
+                          to={`/dashboard/users/${music.user?.username}/scores/${music.id}`}
+                          className="profile-scores-title"
+                        >
+                          <h2 className="profile-scores-title">
+                            {music.title}
+                          </h2>
+                        </Link>
+                        <p className="profile-scores-composer">
+                          {music.composer}
+                        </p>
+                        <div className="profile-scores-genre-instrument">
+                          <p className="profile-scores-instrument">
+                            {music.instrument}
+                          </p>
+                          <span>-</span>
+                          <p className="profile-scores-genre">
+                            {formatGenre(music.genre)}
+                          </p>
+                        </div>
+                        <p className="profile-scores-author">
+                          Uploaded by {music?.user?.name}
+                        </p>
+                        <p className="profile-scores-date">
+                          {new Date(music.uploadedAt).toLocaleDateString(
+                            "en-US",
+                            {
+                              year: "numeric",
+                              month: "long",
+                              day: "numeric",
+                            }
+                          )}
+                        </p>
+                      </li>
+                    )
+                )}
+              </ul>
             ) : (
-              <p>Loading user information...</p>
+              <p className="profile-scores-no-scores">
+                No scores published yet.
+              </p>
             )}
           </div>
-          <div className="profile-body">
-            <div className="profile-scores">
-              <div className="profile-scores-wrapper">
-                <h3>Published Scores ({sheetMusic ? sheetMusic.length : 0})</h3>
-                <div className="profile-scores-filter">
-                  <SelectFilter />
-                </div>
-              </div>
-              {loadingScores ? (
-                <p>Loading scores...</p>
-              ) : sheetMusic && sheetMusic.length > 0 ? (
-                <ul className="profile-scores-list">
-                  {sheetMusic.map(
-                    (music) =>
-                      music.isPublic && (
-                        <li key={music.id} className="profile-scores-item">
-                          <Link
-                            to={`/dashboard/users/${music.user?.username}/scores/${music.id}`}
-                            className="profile-scores-link"
-                          >
-                            <img
-                              src={`https://localhost:7189/${music.previewImage}`}
-                              alt={music.title}
-                              className="profile-scores-image"
-                            />
-                          </Link>
-                          <Link
-                            to={`/dashboard/users/${music.user?.username}/scores/${music.id}`}
-                            className="profile-scores-title"
-                          >
-                            <h2 className="profile-scores-title">
-                              {music.title}
-                            </h2>
-                          </Link>
-                          <p className="profile-scores-composer">
-                            {music.composer}
-                          </p>
-                          <div className="profile-scores-genre-instrument">
-                            <p className="profile-scores-instrument">
-                              {music.instrument}
-                            </p>
-                            <span>-</span>
-                            <p className="profile-scores-genre">
-                              {formatGenre(music.genre)}
-                            </p>
-                          </div>
-                          <p className="profile-scores-author">
-                            Uploaded by {music?.user?.name}
-                          </p>
-                          <p className="profile-scores-date">
-                            {new Date(music.uploadedAt).toLocaleDateString(
-                              "en-US",
-                              {
-                                year: "numeric",
-                                month: "long",
-                                day: "numeric",
-                              }
-                            )}
-                          </p>
-                        </li>
-                      )
-                  )}
-                </ul>
-              ) : (
-                <p className="profile-scores-no-scores">
-                  No scores published yet.
-                </p>
-              )}
+          <div className="profile-favorite-scores">
+            <div className="profile-favorite-scores-title">
+              <h3>
+                Favourite Scores (
+                {favoriteSheetMusic ? favoriteSheetMusic.length : 0})
+              </h3>
             </div>
-            <div className="profile-favorite-scores">
-              <div className="profile-favorite-scores-title">
-                <h3>
-                  Favourite Scores (
-                  {favoriteSheetMusic ? favoriteSheetMusic.length : 0})
-                </h3>
-              </div>
-              {favoriteSheetMusic && favoriteSheetMusic.length > 0 ? (
-                <ul className="profile-favorite-scores-list">
-                  {favoriteSheetMusic.map((music) => (
-                    <li key={music.id} className="profile-favorite-scores-item">
-                      <Link
-                        to={`/dashboard/users/${music.user?.username}/scores/${music.id}`}
-                        className="profile-favorite-scores-link"
-                      >
-                        <img
-                          src={`https://localhost:7189/${music.previewImage}`}
-                          alt={music.title}
-                          className="profile-favorite-scores-image"
-                        />
-                      </Link>
-                      <Link
-                        to={`/dashboard/users/${music.user?.username}/scores/${music.id}`}
-                        className="profile-favorite-scores-title"
-                      >
-                        <h2 className="profile-favorite-scores-title">
-                          {music.title}
-                        </h2>
-                      </Link>
-                      <p className="profile-favorite-scores-composer">
-                        {music.composer}
+            {favoriteSheetMusic && favoriteSheetMusic.length > 0 ? (
+              <ul className="profile-favorite-scores-list">
+                {favoriteSheetMusic.map((music) => (
+                  <li key={music.id} className="profile-favorite-scores-item">
+                    <Link
+                      to={`/dashboard/users/${music.user?.username}/scores/${music.id}`}
+                      className="profile-favorite-scores-link"
+                    >
+                      <img
+                        src={`https://localhost:7189/${music.previewImage}`}
+                        alt={music.title}
+                        className="profile-favorite-scores-image"
+                      />
+                    </Link>
+                    <Link
+                      to={`/dashboard/users/${music.user?.username}/scores/${music.id}`}
+                      className="profile-favorite-scores-title"
+                    >
+                      <h2 className="profile-favorite-scores-title">
+                        {music.title}
+                      </h2>
+                    </Link>
+                    <p className="profile-favorite-scores-composer">
+                      {music.composer}
+                    </p>
+                    <div className="profile-favorite-scores-genre-instrument">
+                      <p className="profile-favorite-scores-instrument">
+                        {music.instrument}
                       </p>
-                      <div className="profile-favorite-scores-genre-instrument">
-                        <p className="profile-favorite-scores-instrument">
-                          {music.instrument}
-                        </p>
-                        <span>-</span>
-                        <p className="profile-favorite-scores-genre">
-                          {formatGenre(music.genre)}
-                        </p>
-                      </div>
-                      <p className="profile-favorite-scores-author">
-                        Uploaded by {music?.user?.name}
+                      <span>-</span>
+                      <p className="profile-favorite-scores-genre">
+                        {formatGenre(music.genre)}
                       </p>
-                      <p className="profile-favorite-scores-date">
-                        {new Date(music.uploadedAt).toLocaleDateString(
-                          "en-US",
-                          {
-                            year: "numeric",
-                            month: "long",
-                            day: "numeric",
-                          }
-                        )}
-                      </p>
-                    </li>
-                  ))}
-                </ul>
-              ) : (
-                <p className="profile-favorite-scores-no-scores">
-                  You haven't added any scores to your favourites yet.
-                </p>
-              )}
-            </div>
+                    </div>
+                    <p className="profile-favorite-scores-author">
+                      Uploaded by {music?.user?.name}
+                    </p>
+                    <p className="profile-favorite-scores-date">
+                      {new Date(music.uploadedAt).toLocaleDateString("en-US", {
+                        year: "numeric",
+                        month: "long",
+                        day: "numeric",
+                      })}
+                    </p>
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <p className="profile-favorite-scores-no-scores">
+                You haven't added any scores to your favourites yet.
+              </p>
+            )}
           </div>
         </div>
       </div>
-      <Toaster />
-    </>
+    </div>
   );
 }
 
